7dada59260c728aa1c1ca90411289801179aa4e2
gaara40 <kumarpalukuru40@gmail.com>
2025-05-29 13:52:24 +0530

    Initial Dashboard Screen

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,11 +1,118 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quick_bill/global_providers/auth_providers.dart';
 
 class DashboardScreen extends ConsumerWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold();
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QuickBill'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await ref.read(authServiceProvider).signOut();
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Total Sales
+            Container(
+              width: double.infinity,
+              constraints: BoxConstraints(minHeight: 100),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: theme.colorScheme.primaryContainer.withValues(
+                  alpha: 0.2,
+                ),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+
+                    children: [
+                      Text(
+                        'Total Sales',
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromRGBO(0, 0, 0, 0.8),
+                        ),
+                      ),
+
+                      Text(
+                        '₹0.00',
+                        style: const TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(0, 0, 0, 0.8),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 18),
+
+            // Action Buttons
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton.icon(
+                    onPressed: () {}, // TODO: Navigate to Create Bill
+                    icon: const Icon(Icons.receipt_long),
+                    label: const Text('New Invoice'),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: ElevatedButton.icon(
+                    onPressed: () {}, // TODO: Navigate to Insights
+                    icon: const Icon(Icons.insights),
+                    label: const Text('Insights'),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      foregroundColor: theme.colorScheme.primary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+
+            // Recent Invoices Heading
+            Text('Recent Invoices', style: theme.textTheme.titleMedium),
+
+            const SizedBox(height: 12),
+
+            // TODO: Replace with dynamic list
+            Expanded(
+              child: ListView(
+                children: const [Text('First Invoice'), Text('First Invoice')],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
